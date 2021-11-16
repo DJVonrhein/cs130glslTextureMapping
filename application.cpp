dd2bac503253512557ff369e527ef7f71be15cdb
Daniel Vonrhein <danjvon@gmail.com>
2021-11-15 23:14:22 -0800

    implemented application.cpp

--- application.cpp
+++ application.cpp
@@ -100,6 +100,14 @@ GLuint loadTarga(const char* filename)
     //glTexImage2D
     //glGenTextures
 
+    glGenTextures(1, &texture_id);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
     return texture_id;
 }
 
