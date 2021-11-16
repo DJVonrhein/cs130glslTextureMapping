1bf3446970cd46e94ddfddcdd65f365fa3588efd
Daniel Vonrhein <danjvon@gmail.com>
2021-11-15 23:40:28 -0800

    trying fragment differently

--- fragment.glsl
+++ fragment.glsl
@@ -8,10 +8,9 @@ void main()
     vec3 L = normalize(diff);
     vec3 normal = normalize(N);
 
-    vec4 tex_color = texture2D(tex, gl_TexCoord[0].st);
+    vec3 tex_color = texture2D(tex, gl_TexCoord[0].st);
     
     float intensity = max(dot(L, normal),0.0);
     gl_FragColor.rgb = intensity * tex_color * gl_LightSource[0].diffuse.rgb + gl_FrontMaterial.ambient.rgb * gl_LightSource[0].ambient.rgb;
     gl_FragColor.a = 1.0;
-    gl_Fragcolor = tex_color;
 }
